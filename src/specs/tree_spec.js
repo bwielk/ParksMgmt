52665c179cef7aef6f72c9d51b734d055811d371
bwielk <bwielk@gmail.com>
2018-07-18 23:09:12 +0100

    Babel, mocha and files connected to each other

---
+++ src/specs/tree_spec.js
@@ -0,0 +1,17 @@
+import {assert} from 'mocha';
+import {Tree} from '../tree.js';
+
+describe("TreeTest", () =>{
+
+	let tree1;
+
+	beforeEach("Setup", () =>{
+		tree1 = new Tree();
+	})
+}),
+
+it("should have id number", () => {
+	assert.equal("123ABC", tree1.getTreeId());
+})
+
+
---
+++ src/tree.js
@@ -0,0 +1,5 @@
+class Tree{
+
+}
+
+export default Tree;
